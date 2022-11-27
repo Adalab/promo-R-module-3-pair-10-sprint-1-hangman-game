600a8ec08c55a5de00cbf55f0133732c95451cc0
noemiyuen <noemiyuen@gmail.com>
2022-11-27 12:09:23 +0100

    Add render errorLeters and Endgame, add new brach

--- src/components/App.js
+++ src/components/App.js
@@ -1,91 +1,119 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import fetchWord from '../services/api';
 import '../styles/App.scss';
 
 function App() {
+  //  pendiente : cuando gane la usuaria poner un cartel de Fin, ganaste y botton reset
 
-//Estados
-  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  //----------Estados----------------
+
+  /*  const [numberOfErrors, setNumberOfErrors] = useState(0); */
   const [lastLetter, setLastLetter] = useState(''); //Para controlar el input
-  const [word, setWord] = useState('katakroker');
+  const [word, setWord] = useState(''); //palabra a adivimar
   const [userLetters, setUserLetters] = useState([]);
 
-//funciones Handle
-const handleClickBtn = () => {
-  setNumberOfErrors(numberOfErrors+1);
-}
+  let numberErrors = 0;
+  let deadText = 'hide';
 
-/* 
-NOTA EXPRESIONES REGULARES
+  /* ----------------------------------------------
+          NOTA EXPRESIONES REGULARES
 
-para aceptar todo el alfabeto Español con sus carácteres especiales, se especifican todos los caracteres
+          para aceptar todo el alfabeto Español con sus carácteres especiales, se especifican todos los caracteres
 
-poodemos usar la negación de los numeros:
-/^[0-9]+$/
-!re.test(event.target.value)
+          poodemos usar la negación de los numeros:
+          /^[0-9]+$/
+          !re.test(event.target.value)
 
-Por otra parte para que acepte "borrar la casilla" hay que incluir antes del $ un *
-*$ en lugar de +$
+          Por otra parte para que acepte "borrar la casilla" hay que incluir antes del $ un *
+          *$ en lugar de +$
 
-función "test" es propia de JS para verificar una expresión regular.
+          función "test" es propia de JS para verificar una expresión regular.
 
-NOTA: VERIFICAR SI ESTA EXPRESIÓN ES LA ADECUADA, FUNCIONAR FUNCIONA
+          NOTA: VERIFICAR SI ESTA EXPRESIÓN ES LA ADECUADA, FUNCIONAR FUNCIONA
+  -------------------------------------------------*/
 
-*/
+  //----------USEEFECT--------------
 
-const handleLastLetter = (event) =>{
-   const re = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü ]*$/; 
-   if (re.test(event.target.value)){//pendiente soporte para no guardar en blanco
-    setLastLetter(event.target.value); 
-    setUserLetters([...userLetters,event.target.value])
-     //Mirar cómo resolverlo!
-   }
-  }
+  useEffect(() => {
+    //cogemos la palabra de la Api
+    fetchWord().then((data) => {
+      const word = data.word;
+      setWord(word);
+    });
+  }, []);
 
+  //----------funciones Handle--------------
 
+  //---HANDLE: BOTON NUMBER ERRORS
+  /* const handleClickBtn = () => {
+    setNumberOfErrors(numberOfErrors + 1);
+  }; */
+
+  //---HANDLE: Letra de la usuaria
+  const handleLastLetter = (event) => {
+    const re = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü ]*$/;
+    if (re.test(event.target.value)) {
+      setLastLetter(event.target.value);
+      if (!userLetters.includes(event.target.value)) {
+        setUserLetters([...userLetters, event.target.value]);
+      }
+    }
+    setLastLetter(''); //para que la usuaria no tenga que borrar el campo cada vez que escribe
+  };
+
+  //-----------funciones-----------------
+  //---fc: Solución
   const renderSolutionLetters = () => {
     const wordLetters = word.split('');
-    console.log(wordLetters);
-    console.log(userLetters);
-
-     /*const errorLetter = userLetters.filter((letter2)=> { 
-      return letter2.includes(wordLetters)})
-      console.log(`error ${errorLetter}`); */
-
-    return wordLetters.map ((letter, index) => {
-      const errorLetters =[] 
-      userLetters.filter((letter2)=> { 
-      return errorLetters.push (letter2.includes(letter))
+    return wordLetters.map((letter, index) => {
+      //aprovechamos el map para hacer un filter dentro
+      const drawletter = userLetters.filter((userL) =>
+        userL.includes(letter) ? userL : ''
+      );
+      return (
+        <li key={index} className="letter">
+          {drawletter}
+        </li>
+      );
     });
+  };
 
-      console.log(`error ${errorLetters}`);
-      return <li key={index}  className="letter"></li>;
-     
-  });
-}
+  //---fc: fallos
 
+  const renderErrorLetters = () => {
+    const errors = userLetters.filter((userL) => !word.includes(userL));
 
+    return errors.map((error, index) => {
+      numberErrors++;
 
-/* const renderErrorLetters =() => {  */
-    // array: userLetters lo que escribe
-    //array: solución wordletters
-/*   return const errorLetter = userLetters.filter((letter)=> {!letter.includes(wordLetters)})
-    
-  } */
- 
+      if (numberErrors < 13) {
+        return (
+          <li key={index} className="letter">
+            {' '}
+            {error}{' '}
+          </li>
+        );
+      } else {
+        deadText = '';
+      }
+    });
+  };
 
+  //-----------HTML------------------
   return (
-  <div className="page">
-<header>
-  <h1 className="header__title">Juego del ahorcado</h1>
-</header>
-<main className="main">
-  <section>
-    <div className="solution">
-      <h2 className="title">Solución:</h2>
-    
-      <ul className="letters">
-      {renderSolutionLetters()}
-        {/* <li className="letter">k</li>
+    <div className="page">
+      <header>
+        <h1 className="header__title">Juego del ahorcado</h1>
+      </header>
+      <main className="main">
+        <section>
+          <div className="solution">
+            <h2 className="title">Solución:</h2>
+
+            <ul className="letters">
+              {renderSolutionLetters()}
+              {/* <li className="letter">k</li>
         <li className="letter">a</li>
         <li className="letter"></li>
         <li className="letter">a</li>
@@ -95,56 +123,57 @@ const handleLastLetter = (event) =>{
         <li className="letter">k</li>
         <li className="letter">e</li>
         <li className="letter">r</li> */}
-      </ul>
-    </div>
-    <div className="error">
-      <h2 className="title">Letras falladas:</h2>
-      <ul className="letters">
-     {/*  {renderErrorLetters()} */}
-   {/*      <li className="letter">f</li>
+            </ul>
+          </div>
+          <div className="error">
+            <h2 className="title">Letras falladas:</h2>
+            <ul className="letters">
+              {renderErrorLetters()}
+              {/*      <li className="letter">f</li>
         <li className="letter">q</li>
         <li className="letter">h</li>
         <li className="letter">p</li>
         <li className="letter">x</li> */}
-      </ul>
+            </ul>
+            <p className={`title_dead ${deadText}`}> FIN ! Has muerto!</p>
+          </div>
+          <form className="form">
+            <label className="title" htmlFor="last-letter">
+              Escribe una letra:
+            </label>
+            <input
+              autoComplete="off"
+              className="form__input"
+              maxLength="1"
+              type="text"
+              name="last-letter"
+              id="last-letter"
+              value={lastLetter}
+              onChange={handleLastLetter}
+              pattern={'^[a-zA-Z]+$'}
+            />
+          </form>
+        </section>
+        {/*    <button className="btn" onClick={handleClickBtn}>
+          Incrementar
+        </button> */}
+        <section className={`dummy error-${numberErrors}`}>
+          <span className="error-13 eye"></span>
+          <span className="error-12 eye"></span>
+          <span className="error-11 line"></span>
+          <span className="error-10 line"></span>
+          <span className="error-9 line"></span>
+          <span className="error-8 line"></span>
+          <span className="error-7 line"></span>
+          <span className="error-6 head"></span>
+          <span className="error-5 line"></span>
+          <span className="error-4 line"></span>
+          <span className="error-3 line"></span>
+          <span className="error-2 line"></span>
+          <span className="error-1 line"></span>
+        </section>
+      </main>
     </div>
-    <form className="form">
-      <label className="title" htmlFor="last-letter">Escribe una letra:</label>
-      <input
-        autoComplete="off"
-        className="form__input"
-        maxLength="1"
-        type="text"
-        name="last-letter"
-        id="last-letter"
-        value={lastLetter}
-        onChange={handleLastLetter}
-        pattern = {"^[a-zA-Z]+$"}
-
-      />
-    </form>
-  </section>
-  <button className="btn" onClick={handleClickBtn}>Incrementar</button>
-  <section className={`dummy error-${numberOfErrors}`}>
-    <span className="error-13 eye"></span>
-    <span className="error-12 eye"></span>
-    <span className="error-11 line"></span>
-    <span className="error-10 line"></span>
-    <span className="error-9 line"></span>
-    <span className="error-8 line"></span>
-    <span className="error-7 line"></span>
-    <span className="error-6 head"></span>
-    <span className="error-5 line"></span>
-    <span className="error-4 line"></span>
-    <span className="error-3 line"></span>
-    <span className="error-2 line"></span>
-    <span className="error-1 line"></span>
-  </section>
-</main>
-</div>
-
-
-
   );
 }
 
